57bba777b4afae5526fb12ba72ef131c182e0b25
Andrew Morris <voltrevo@gmail.com>
2016-01-25 11:15:24 +1100

    yo node

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+var tokalite = require('../lib');
+
+describe('tokalite', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
